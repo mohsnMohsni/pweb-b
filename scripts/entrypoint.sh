1574434761f3dbe6be403376c96d0e9fecbaa02b
Mohsen Mohseni <mohsenmohseni.co@gmail.com>
2022-12-04 16:26:36 +0330

    fix: run gunicorn as python module

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -3,4 +3,4 @@
 while ! python manage.py sqlflush > /dev/null 2>&1; do echo "sleep" && sleep 1; done;
 
 python manage.py migrate --no-input
-gunicorn --bind 0.0.0.0:8000 core.wsgi
+pytohn -m gunicorn --bind 0.0.0.0:8000 core.wsgi
